9d02e049c74419a07a17d79a2ab4357b0ef09ad7
Fosuri <134892507+fosuri@users.noreply.github.com>
2026-03-23 20:04:02 +0200

    moderator page

---
+++ app/moderator/page.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db/db";
+import { user } from "@/lib/db/auth-schema";
+import { eq } from "drizzle-orm";
+import { getSession } from "@/lib/auth/auth-server";
+import { redirect } from "next/navigation";
+
+export default async function ModeratorPage() {
+  const session = await getSession();
+  if (!session) {
+    redirect("/");
+  }
+
+  const dbUser = await db.query.user.findFirst({
+    where: eq(user.id, session.user.id),
+  });
+
+  if (!dbUser || (dbUser.role !== "moderator" && dbUser.role !== "admin")) {
+    redirect("/");
+  }
+
+  return (
+    <div>
+      <h1>Moderator Dashboard</h1>
+      <p>Username: {dbUser.name}</p>
+      <p>Email: {dbUser.email}</p>
+      <p>Role: {dbUser.role}</p>
+    </div>
+  );
+}
