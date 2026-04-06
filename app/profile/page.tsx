08d67c23a1914e6d2f7c8f5d3a2ff941efd477d9
Fosuri <134892507+fosuri@users.noreply.github.com>
2026-04-07 02:20:18 +0300

    :rage2:

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -34,7 +34,7 @@ export default async function ProfilePage() {
           </div>
 
           {userPosts.length ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {userPosts.map((post) => (
                 <FeedPostCard
                   key={post.id}
